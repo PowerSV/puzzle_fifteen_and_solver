3d58afb13764406849cd789cb970fe4ef89d7963
ballo <ballo.aleksej@gmaiol.com>
2022-05-17 00:59:28 +0300

    fix solver

--- src/sample/solver/Solver.java
+++ src/sample/solver/Solver.java
@@ -3,9 +3,11 @@ package sample.solver;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.Set;
 
 // visited to hashset
 // Node add weight
@@ -19,11 +21,12 @@ public class Solver {
     }
 
     public Solver(SolverState startState) {
-        Queue<Node> openStates = new PriorityQueue<>(Comparator.comparing(this::getWeight));
-        List<Integer> visitedStates = new ArrayList<>();
-        Node startNode = new Node(null, startState);
+        Queue<Node> openStates = new PriorityQueue<>(Comparator.comparing(Node::getWeight));
+        Set<SolverState> visitedStates = new HashSet<>();
 
+        Node startNode = new Node(null, startState);
         openStates.add(startNode);
+
         while (!openStates.isEmpty()) {
             Node currentNode = openStates.poll();
 
@@ -31,29 +34,30 @@ public class Solver {
                 fillResult(new Node(currentNode, currentNode.state));
                 return;
             }
-            visitedStates.add(currentNode.state.hashCode());
 
             for (SolverState neighbor : currentNode.state.getNeighbors()) {
-                if (neighbor != null && !visitedStates.contains(neighbor.hashCode())) {
-                    openStates.add(new Node(currentNode, neighbor));
+                if (neighbor != null && !visitedStates.contains(neighbor)) {
+                    Node temp = new Node(currentNode, neighbor);
+                    openStates.add(temp);
                 }
             }
+            visitedStates.add(currentNode.state);
         }
     }
 
-    private int getWeight(Node node) {
-        return node.state.getHeuristic() + calculatePath(node);
-    }
+//    private int getWeight(Node node) {
+//        return node.getG() + node.state.getHeuristic();
+//    }
 
-    private int calculatePath(Node nodeState) {
-        Node currentNode = nodeState.parent;
-        int pathCounter = 0;
-        while (currentNode != null) {
-            pathCounter++;
-            currentNode = currentNode.parent;
-        }
-        return pathCounter;
-    }
+//    private int calculatePath(Node nodeState) {
+//        Node currentNode = nodeState.parent;
+//        int pathCounter = 0;
+//        while (currentNode != null) {
+//            pathCounter++;
+//            currentNode = currentNode.parent;
+//        }
+//        return pathCounter;
+//    }
 
     private void fillResult(Node node) {
         Node currentNode = node.parent;
@@ -66,10 +70,22 @@ public class Solver {
     private static class Node {
         private final Node parent;
         private final SolverState state;
+        private int g;
+        private int weight;
+
+        public int getG() {
+            return g;
+        }
+
+        public int getWeight() {
+            return weight;
+        }
 
         public Node(Node parent, SolverState state) {
             this.parent = parent;
             this.state = state;
+            this.g = parent == null ? 0 : parent.getG() + 1;
+            this.weight = g + state.getHeuristic();
         }
     }
 }
