06ad333c8c31377169da3d803503048d08795c17
ballo <ballo.aleksej@gmaiol.com>
2022-05-23 17:24:45 +0300

    v1.2

--- src/sample/Field.java
+++ src/sample/Field.java
@@ -24,20 +24,6 @@ public class Field {
         score = 0;
     }
 
-//     один из примеров на котором решатель падает с outOfMemory
-//
-//    public Field() {
-//        field = new byte[][]{
-//                {1, 7, 13, 14},
-//                {2, 3, 6, 9},
-//                {11, 5, 12, 15},
-//                {4, 0, 10, 8}
-//        };
-//        zeroX = 3;
-//        zeroY = 1;
-//        score = 0;
-//    }
-
     private void initializeField() {
         field = new byte[SIZE][SIZE];
         byte temp = 0;
